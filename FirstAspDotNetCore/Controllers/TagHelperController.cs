29b5421325f2fd99023743687b10e2838efbd178
Fatemeh Montazeri <montazeri_f@hotmail.com>
2019-11-17 03:21:41 +0330

    Using tagHelper for anchor

---
+++ FirstAspDotNetCore/Controllers/TagHelperController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstAspDotNetCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstAspDotNetCore.Controllers
+{
+    public class TagHelperController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Index2()
+        {
+            return View();
+        }
+
+        public IActionResult Index3(int id)
+        {
+            return View();
+        }
+
+        public  IActionResult Index4(PersonModel person)
+        {
+            return View(person);
+        }
+    }
+}
