3552aaecbe06dd9aa1dd224fcc658fefff4f67e9
Fatemeh Montazeri <montazeri_f@hotmail.com>
2019-11-16 13:30:13 +0330

    add JsonController

---
+++ FirstAspDotNetCore/Controllers/JsonController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstAspDotNetCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace FirstAspDotNetCore.Controllers
+{
+    public class JsonController
+    {
+        public JsonResult Get()
+        {
+            var persons = new List<PersonModel>()
+            {
+                new PersonModel(){ Name = "Fatemeh" , Family = "Montazeri"} ,
+                new PersonModel(){Name = "Zahra" , Family = "Montazeri"} ,
+                new PersonModel(){Name = "Ali" ,Family = "Montazeri"}
+            };
+
+            return new JsonResult(persons);
+        }
+    }
+}
