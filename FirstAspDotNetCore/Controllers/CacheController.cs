74b7d388cd1098fba2a5eae05ab08b2e2dbb57c3
Fatemeh Montazeri <montazeri_f@hotmail.com>
2019-11-17 02:40:51 +0330

    Using tagHelper cache

---
+++ FirstAspDotNetCore/Controllers/CacheController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstAspDotNetCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstAspDotNetCore.Controllers
+{
+    public class CacheController : Controller
+    {
+        public IActionResult Index()
+        {
+            var person = new PersonModel() {Name = "Fatemeh", Family = "Montazeri"};
+            ViewBag.Person = person;
+            return View();
+        }
+
+        public IActionResult Get()
+        {
+            return View("Index");
+        }
+    }
+}
