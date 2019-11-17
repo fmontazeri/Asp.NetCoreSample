e400cbc3105d9a825b67ce1fb18f86a18fa9cff9
Fatemeh Montazeri <montazeri_f@hotmail.com>
2019-11-17 20:59:28 +0330

    create form using TagHelpers

--- FirstAspDotNetCore/Controllers/FormController.cs
+++ FirstAspDotNetCore/Controllers/FormController.cs
@@ -22,15 +22,41 @@ namespace FirstAspDotNetCore.Controllers
         {
 
             //File --> Base64(string)
-            using (var ms = new MemoryStream())
+            if (File != null)
             {
-                File.CopyTo(ms);
-                byte[] imageBytes = ms.ToArray();
-                var imageBase64 = Convert.ToBase64String(imageBytes);
-                model.Image = $"data:{File.ContentType};base64,{imageBase64}";
+                using (var ms = new MemoryStream())
+                {
+                    File.CopyTo(ms);
+                    byte[] imageBytes = ms.ToArray();
+                    var imageBase64 = Convert.ToBase64String(imageBytes);
+                    model.Image = $"data:{File.ContentType};base64,{imageBase64}";
+                }
             }
             ViewBag.Person = model;
             return View();
+
+        }
+
+        public IActionResult ByTagHelper()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ByTagHelper(PersonModel model , IFormFile File)
+        {
+            if (File != null)
+            {
+                using (var ms = new MemoryStream())
+                {
+                    File.CopyTo(ms);
+                    var imagesArr = ms.ToArray();
+                    var imageBase64 = Convert.ToBase64String(imagesArr);
+                    model.Image = $"data:{File.ContentType};base64,{imageBase64}";
+                }
+          
+            }
+            return View(model);
 
         }
     }
