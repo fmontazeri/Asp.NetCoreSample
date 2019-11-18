82bf6b390826bcf2fb6e493c977f41170db5cfbf
Fatemeh Montazeri <montazeri_f@hotmail.com>
2019-11-18 16:42:58 +0330

    using attributes for routing

--- FirstAspDotNetCore/Controllers/Route.cs
+++ FirstAspDotNetCore/Controllers/Route.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Server.HttpSys;
 
 namespace FirstAspDotNetCore.Controllers
 {
+  
+   // [Route("admin/[Controller]/[action]/{id?}/{title?}")]
     public class Route : Controller
     {
 
@@ -31,6 +34,12 @@ namespace FirstAspDotNetCore.Controllers
             return View();
         }
         public IActionResult Route4(int id , string title)
+        {
+            return View();
+        }
+
+        [Route("[Controller]/MyPage/{id:weekday?}")]
+        public IActionResult Route5()
         {
             return View();
         }
